f2483735ac7f80cf92435cf415170784b10effa3
ron.hagafny@darrow.ai <ron.hagafny@darrow.ai>
2025-05-06 18:14:54 +0300

    initial flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const fetchHelloMessage = async () => {
+      try {
+        const response = await fetch("/api/hello");
+        const data = await response.json();
+        console.log(data.message);
+      } catch (error) {
+        console.error("Error fetching the hello message:", error);
+      }
+    };
+
+    fetchHelloMessage();
+  }, []);
+
+  
   return (
     <div className="App">
       <main>News Room</main>
--- server/server.ts
+++ server/server.ts
@@ -1,42 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import NewsAPI from "newsapi";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT || "5000");
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY || "");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello from the server!" });
-});
-
-app.post("/api/chat", (req: Request, res: Response) => {
-  const userMessage = req.body.message;
-  const response = `This is a streaming response to: "${userMessage}". It will be sent word by word.`;
-  const words = response.split(" ");
-
-  // Add a 1-second delay before starting to send the response
-  setTimeout(() => {
-    res.writeHead(200, {
-      "Content-Type": "text/plain",
-      "Transfer-Encoding": "chunked",
-    });
-
-    let index = 0;
-    const intervalId = setInterval(() => {
-      if (index < words.length) {
-        res.write(words[index] + " ");
-        index++;
-      } else {
-        clearInterval(intervalId);
-        res.end();
-      }
-    }, 200);
-  }, 1000);
+app.get("/api/hello", async (req: Request, res: Response) => {
+  try {
+    // const response = await fetch(`${NEWS_SERVICE_URL}/health`);
+    // const status = response.statusText;
+    res.json({ message: process.env.NEWS_API_KEY });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching news service status" });
+  }
 });
 
 app.listen(port, () => {
