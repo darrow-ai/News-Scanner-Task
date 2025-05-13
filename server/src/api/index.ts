c0c1db5328ca50ad7c609af3b0959a60c17986cc
ron.hagafny@darrow.ai <ron.hagafny@darrow.ai>
2025-05-13 23:11:42 +0300

    cleaned up empty

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -6,6 +6,10 @@ router.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Darrow's News API");
 });
 
+router.get("/categories", async (req: Request, res: Response) => {
+  // TODO: Implement the categories API
+});
+
 router.get("/news", async (req: Request, res: Response) => {
   // TODO: Implement the news API
 });
